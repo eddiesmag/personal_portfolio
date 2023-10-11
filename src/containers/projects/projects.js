3e892097a598befb8e440d7dc3a59727a1cde218
eddiesmag <eddiesmag@gmail.com>
2023-10-11 15:44:35 +0300

    chore:add github repo card

--- src/containers/projects/projects.js
+++ src/containers/projects/projects.js
@@ -1,34 +1,49 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Slide } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Fade, Grid } from '@mui/material';
 import { useInView } from 'react-intersection-observer';
 import GithubRepoCard from '../../components/projects/githubRepoCard';
 
 const Projects = () => {
   const [isInView, setIsInView] = useState(false);
-  const ref = useRef(null);
-  const [inView] = useInView({
+
+  const [ref, inView] = useInView({
     threshold: 0,
     delay: 1000,
     triggerOnce: true,
   });
 
   useEffect(() => {
-    if (inView) {
-      setIsInView(true);
-    }
+    const setInView = () => {
+      if (inView) {
+        return setIsInView(true);
+      }
+    };
+    setInView();
   }, [inView]);
 
   return (
     <div ref={ref}>
-      {ref.current && (
-        <Slide in={isInView} container={ref.current}>
-          <div style={{ display: 'flex' }}>
-            {[1, 2].map((i) => (
-              <GithubRepoCard key={i} />
-            ))}
-          </div>
-        </Slide>
-      )}
+      <Fade in={isInView} timeout={1000}>
+        <Grid
+          container
+          direction="row"
+          spacing={2}
+          m={0}
+          p={0}
+          pr={5}
+          pl={3}
+          sx={{
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+          }}
+        >
+          {[12, 4, 4].map((item, i) => (
+            <Grid key={i} item lg={6} md={6} xs={12}>
+              <GithubRepoCard />
+            </Grid>
+          ))}
+        </Grid>
+      </Fade>
     </div>
   );
 };

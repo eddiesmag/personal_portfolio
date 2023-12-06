80d10abbdb12b21952f99e25c1dd161d19a4c546
eddiesmag <eddiesmag@gmail.com>
2023-12-06 19:06:32 +0300

    chore:add theme to button

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button } from '@mui/material';
-
-import './button.scss';
+import StyleContext from '../../contexts/StyleContext';
 
 const ButtonComp = ({ text, href, newTab, btnSize }) => {
+  const { isDark } = useContext(StyleContext);
   return (
-    <Button variant="contained" href={href} target={newTab && '_blank'} size={btnSize}>
+    <Button
+      variant="contained"
+      href={href}
+      target={newTab && '_blank'}
+      size={btnSize}
+      sx={{
+        color: 'primary.contrastText',
+        bgcolor: 'primary.main',
+        '&:hover': {
+          transition: 0.3,
+          bgcolor: isDark ? 'primary.dark' : 'primary.light',
+          boxShadow: 5
+        }
+      }}>
       {text}
     </Button>
   );
